0ba6f8ae52466ef02d4001d8920471e93ed83888
Luca Schwarz <lschwarz6605@gmail.com>
2026-01-09 11:28:12 +0100

    rename package structure to 'de.hhu.propra'

--- src/main/java/de/hhu/propra/PropraProjektPlaygroundApplication.java
+++ src/main/java/de/hhu/propra/PropraProjektPlaygroundApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package de.hhu.propra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/de/hhu/propra/PropraProjektPlaygroundApplicationTests.java
+++ src/test/java/de/hhu/propra/PropraProjektPlaygroundApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package de.hhu.propra;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
