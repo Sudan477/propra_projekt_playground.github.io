c3b6dd31e20ddf5e2c1ecc4f701d4680f7c88c1c
Luca Schwarz <lschwarz6605@gmail.com>
2026-01-29 12:23:36 +0100

    set up starter module and implement static resources

---
+++ starter/ApplicationStarter.java
@@ -0,0 +1,7 @@
+
+@SpringBootApplication
+public class ApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityIntroApplication.class, args);
+    }
